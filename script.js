f0abe2b5654883037a3e0b35c8bf01c15be29dc5
Chris Argyros <cargyros430@gmail.com>
2022-08-06 22:24:38 -0500

    grid slider function

--- script.js
+++ script.js
@@ -27,3 +27,20 @@ function getRandomColor(){
       }
       return color;
 }
+
+const slider = document.querySelector('#slider')
+const screenVal = document.querySelector('.value');
+slider.addEventListener('input', function(){
+    let val = document.getElementById('slider').value;
+    screenVal.textContent = val;
+    removeAllChildNodes(grid);
+    grid.setAttribute('style', `grid-template-columns: repeat(${val}, 2fr); grid-template-rows: repeat(${val}, 2fr);`);
+    for (let i = 0; i < val*val; i++) {
+        const div = document.createElement('div');
+        div.classList.add('cell');
+        div.addEventListener('mouseover', function(event){
+            event.target.style.backgroundColor = 'black';
+        })
+        grid.appendChild(div); 
+    }
+});
