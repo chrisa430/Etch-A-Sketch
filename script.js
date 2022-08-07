e6d53e1f47ce0e047481358400a9f7f9eefffbdc
Chris Argyros <cargyros430@gmail.com>
2022-08-06 22:13:14 -0500

    random color function

--- script.js
+++ script.js
@@ -17,3 +17,13 @@ function removeAllChildNodes(parent){
         parent.removeChild(parent.firstChild);
     }
 }
+
+
+function getRandomColor(){
+    let letters = '0123456789ABCDEF';
+    let color = '#';
+    for (var i = 0; i < 6; i++) {
+        color += letters[Math.floor(Math.random() * 16)];
+      }
+      return color;
+}
