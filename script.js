0d5f210e62d8e452d0048f8135d327803a2830d4
Chris Argyros <cargyros430@gmail.com>
2022-08-06 21:33:06 -0500

    add grid

---
+++ script.js
@@ -0,0 +1,12 @@
+const grid = document.querySelector('.grid');
+
+createGrid = () => {
+for (let i = 0; i < 256; i++) {
+    const div = document.createElement('div');
+    div.classList.add('cell');
+    div.addEventListener('mouseover', function(event){
+        event.target.style.backgroundColor = 'black';
+    })
+    grid.appendChild(div); 
+}
+};
